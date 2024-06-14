a082276305369d34154a929e325075ee6f5c49bf
LuisHuacca <fhuacca@gmail.com>
2024-06-13 22:26:38 -0500

    Se ha agregado un ejercicio de estructuras selectivas

---
+++ ejercicioselectivas1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float calificacion1, calificacion2, calificacion3;
+    
+    cout<<"Ingrese la primera calificacion: ";
+    cin>>calificacion1;
+    
+    cout<<"Ingrese la segunda calificacion: ";
+    cin>>calificacion2;
+    
+    cout<<"Ingrese la tercera calificacion: ";
+    cin >> calificacion3;
+    
+    float promedio = (calificacion1 + calificacion2 + calificacion3) / 3;
+    
+    if (promedio >= 70) {
+        cout<<"El alumno aprueba el curso con un promedio de "<<promedio <<"."<<endl;
+    } else {
+        cout<<"El alumno reprueba el curso con un promedio de "<<promedio<<"."<<endl;
+    }
+    
+    return 0;
+}
+
